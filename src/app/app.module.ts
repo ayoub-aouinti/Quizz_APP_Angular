6ec3ae589e61678f95bc2c7e99200891cb8bcf15
Ayoub <ayoubaouinti1990@gmail.com>
2023-04-21 19:55:55 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomComponent } from '.d:/repos/Quizz_App_Angular/Quizz_APP_Angular/src/welcom/welcom.component';
+import { WelcomComponent } from './welcom/welcom.component';
 import { QuestionComponent } from './question/question.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [		
+  declarations: [	
     AppComponent,
       WelcomComponent,
-      QuestionComponent
+      QuestionComponent,
+      HeaderComponent
    ],
   imports: [
     BrowserModule,
